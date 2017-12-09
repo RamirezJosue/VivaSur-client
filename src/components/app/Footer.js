30274ab18cc41a2d01c24d94cfab67abeb1866a5
RamirezJosue <xdemobtkx@gmail.com>
2017-12-09 11:09:18 -0600

    subiendo

---
+++ src/components/app/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <div className="footer">
+                    <div className="footer_seccion">
+                        <p>Todos los derechos reservados © 2017 - By Webtilia</p>
+                    </div>
+
+                </div>
+            </footer>
+        )
+    }
+}
+export default (Footer)
---
+++ src/components/app/Header.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+
+import {
+  Link,
+} from 'react-router-dom'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="encabezado  navbar fixed-top ">
+        <div className="container">
+          <a className="logo">
+            <img src="" alt="Logo del sitio" />
+          </a>
+          <button type="button" className="boton-buscar">
+            <i class="fa fa-search" aria-hidden="true"></i>
+          </button>
+          <button type="button" className="boton-menu">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+          </button>
+          <form>
+            <div className="contenedor-bloque-buscar">
+              <input type="text" placeholder="Buscar..." />
+              <input type="submit" value="Buscar" />
+            </div>
+          </form>
+          <nav id="menu-principal" className="ml-auto" >
+            <ul>
+              <li><Link to="/home">Inicio</Link></li>
+              <li><Link to="/login">login</Link></li>
+              <li><Link to='/catalogo/habitacion/list'>Habitaciones</Link></li>
+            </ul>
+
+          </nav>
+        </div>
+      </header>
+    );
+  }
+}
+export default (Header)
