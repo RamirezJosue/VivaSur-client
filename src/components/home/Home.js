fba613881c7c26f8eb60459c464d3f83afcd50c2
RamirezJosue <xdemobtkx@gmail.com>
2017-12-09 17:25:45 -0600

    subiendo1

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,34 +2,27 @@ import React, { Component } from 'react';
 import {
     Link
 } from 'react-router-dom'
+
+//
+import '../css/style.css';
+import '../images/font-awesome/css/font-awesome.min.css'
+
 class Home extends Component {
 
     render() {
         return (
-
-            <div className="imgbackground portada" >
-                <div className="formulario">
-                <input className="form" type="date" name="bday" min="2000-01-02" >
-                 </input>
-                 <input className="form" type="date" name="bday" min="2000-01-02">
-                </input>
-
-                <select className="form">
-                    <option value="Habitacion">Habitacion</option>
-                    <option value="Individual">Individual</option>
-                    <option value="Matrimonial">Matrimonial</option>
-                    <option value="Triple">Triple</option>
-                </select>
-                <Link exact to='/person/form' activeClassName="selected"><input className="form btn" type="submit" name="" value="Reservar" /></Link>
-
+            <div>
+                <div className="texto-encabezado text-center" >
+                    <div className="container">
+                        <h1 className="display-4">Diseno de APPS para empresas</h1>
+                        <p> Creamos aplicaciones  segun las necesidadesy requirimientos de nuestros clientes.</p>
+                        <Link to="#" className=" btn btn-primary btn-lg" > Ponte en contacto </Link>
+                    </div>
                 </div>
-                <div className="text">
-                <h1>Te ayudamos a que pases una noche increible en nuestras fabulosas habitaciones</h1>
+                <div className="flecha-bajar">
+                    <Link to="">Boton bajar</Link>
                 </div>
-
-
             </div>
-
 
 
         );
